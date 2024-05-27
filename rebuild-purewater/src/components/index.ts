2522c00dd5c39d2f7137dd6734b43099a6b855d0
isnendyankp <isnendyankp@gmail.com>
2024-05-28 05:13:04 +0700

    add distributor file navbar component

--- rebuild-purewater/src/components/index.ts
+++ rebuild-purewater/src/components/index.ts
@@ -4,3 +4,4 @@ export { default as Card } from './Card';
 export { default as Text } from './Text';
 export { default as Button } from './Button';
 export { default as ScrollToTopButton } from './ScrollToTopButton';
+export { default as Navbar } from './Navbar';
