141041e6c9166421b61fd6b8ab96580d6c334edb
isnendyankp <isnendyankp@gmail.com>
2024-05-29 05:24:35 +0700

    add div inside nav

--- rebuild-purewater/src/components/Navbar/index.tsx
+++ rebuild-purewater/src/components/Navbar/index.tsx
@@ -15,7 +15,9 @@ const Navbar = () => {
   return (
     // Navbar component starts here
     <nav className="bg-white border border-gray-200 dark:border-gray-700 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800 shadow sticky top-0 z-[100]">
-
+      <div className="container flex flex-wrap justify-between items-center mx-auto">
+        
+      </div>
     </nav>
   );
 }
