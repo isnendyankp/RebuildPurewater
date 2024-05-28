4d7e07a6da6e4c87f0e2b02ab932362ef6493f1a
isnendyankp <isnendyankp@gmail.com>
2024-05-29 05:23:24 +0700

    chore: Update Navbar component with new styles

--- rebuild-purewater/src/components/Navbar/index.tsx
+++ rebuild-purewater/src/components/Navbar/index.tsx
@@ -13,8 +13,11 @@ const Navbar = () => {
   };
 
   return (
-    <div>index</div>
-  )
+    // Navbar component starts here
+    <nav className="bg-white border border-gray-200 dark:border-gray-700 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800 shadow sticky top-0 z-[100]">
+
+    </nav>
+  );
 }
 
 export default Navbar
