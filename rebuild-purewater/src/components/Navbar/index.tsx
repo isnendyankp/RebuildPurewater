d48ceb16a3ff2a4ef1292f579942ad8a0a2c573c
isnendyankp <isnendyankp@gmail.com>
2024-05-29 05:20:56 +0700

    cr8 function to toggle mobile menu

--- rebuild-purewater/src/components/Navbar/index.tsx
+++ rebuild-purewater/src/components/Navbar/index.tsx
@@ -6,7 +6,12 @@ const Navbar = () => {
 
   // This is a state for mobile menu
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-  
+
+  // This is a function to toggle mobile menu
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
     <div>index</div>
   )
