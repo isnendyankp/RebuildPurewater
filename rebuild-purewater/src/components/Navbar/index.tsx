e54158ca66421584432b41abcd525d4647c096e1
isnendyankp <isnendyankp@gmail.com>
2024-05-29 05:26:41 +0700

    refactor: Add Pure Water link to Navbar component

--- rebuild-purewater/src/components/Navbar/index.tsx
+++ rebuild-purewater/src/components/Navbar/index.tsx
@@ -16,7 +16,12 @@ const Navbar = () => {
     // Navbar component starts here
     <nav className="bg-white border border-gray-200 dark:border-gray-700 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800 shadow sticky top-0 z-[100]">
       <div className="container flex flex-wrap justify-between items-center mx-auto">
-        
+        {/* This link for back to home */}
+        <Link href="/" className="flex items-center">
+          <span className="self-center text-xl font-semibold whitespace-nowrap text-black">
+            Pure Water
+          </span>
+        </Link>
       </div>
     </nav>
   );
