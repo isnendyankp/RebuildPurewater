dbd1f740107f1d983d50225dc082eb20482b67e6
isnendyankp <isnendyankp@gmail.com>
2024-05-30 05:19:23 +0700

    Add link back to home for Home list @navbar desktop

--- rebuild-purewater/src/components/Navbar/index.tsx
+++ rebuild-purewater/src/components/Navbar/index.tsx
@@ -60,7 +60,7 @@ const Navbar = () => {
           {/* This ul for menu items */}
           <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
             <li>
-              Home
+              <Link href="/">Home</Link>
             </li>
           </ul>
         </div>
