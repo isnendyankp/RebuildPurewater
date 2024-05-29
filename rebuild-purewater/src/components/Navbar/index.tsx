0d94fb29f1f88f7560afa85eefc5965ee9848c21
isnendyankp <isnendyankp@gmail.com>
2024-05-30 05:15:31 +0700

    add ul for menu items to desktop navbar

--- rebuild-purewater/src/components/Navbar/index.tsx
+++ rebuild-purewater/src/components/Navbar/index.tsx
@@ -57,7 +57,10 @@ const Navbar = () => {
           }`}
           id="mobile-menu"
         >
+          {/* This ul for menu items */}
+          <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
 
+          </ul>
         </div>
       </div>
     </nav>
