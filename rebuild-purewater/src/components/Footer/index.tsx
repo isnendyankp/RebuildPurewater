e339394240ae35171888cdc849dc2db011c50cad
isnendyankp <isnendyankp@gmail.com>
2024-06-04 05:03:26 +0700

    add div wrapper 1 @footer Component

--- rebuild-purewater/src/components/Footer/index.tsx
+++ rebuild-purewater/src/components/Footer/index.tsx
@@ -3,7 +3,12 @@ import React from 'react'
 const Footer = () => {
   return (
     <div id="footer" className="flex items-end w-full bg-white">
-      <footer>index</footer>
+      <footer>
+        {/* div wrapper 1 */}
+        <div className="container flex flex-col flex-wrap px-5 py-24 mx-auto md:items-center lg:items-start md:flex-row md:flex-no-wrap">
+          index
+        </div>
+      </footer>
     </div>
   );
 }
