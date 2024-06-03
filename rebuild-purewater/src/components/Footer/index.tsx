edae971d49ac39a5b0591c698ca36cc235c7b8e1
isnendyankp <isnendyankp@gmail.com>
2024-06-04 05:05:18 +0700

    add ublished by Purewater section @footer Component

--- rebuild-purewater/src/components/Footer/index.tsx
+++ rebuild-purewater/src/components/Footer/index.tsx
@@ -8,6 +8,10 @@ const Footer = () => {
         <div className="container flex flex-col flex-wrap px-5 py-24 mx-auto md:items-center lg:items-start md:flex-row md:flex-no-wrap">
           index
         </div>
+        {/* Published by Purewater section */}
+        <div className="bg-gray-300">
+          © 2024. Published By Pure Water. All rights reserved.
+        </div>
       </footer>
     </div>
   );
