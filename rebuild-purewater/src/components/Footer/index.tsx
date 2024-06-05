ad947a4ddc636dcf1d756c2f7cf6581d7c86f7b7
isnendyankp <isnendyankp@gmail.com>
2024-06-05 08:09:40 +0700

    Add main Footer Content inside wrapper

--- rebuild-purewater/src/components/Footer/index.tsx
+++ rebuild-purewater/src/components/Footer/index.tsx
@@ -4,9 +4,12 @@ const Footer = () => {
   return (
     <div id="footer" className="flex items-end w-full bg-white">
       <footer>
-        {/* div wrapper 1 */}
+        {/* div for wrapper outer main footer content */}
         <div className="container flex flex-col flex-wrap px-5 py-24 mx-auto md:items-center lg:items-start md:flex-row md:flex-no-wrap">
-          index
+          {/* Main Footer Content */}
+          <div >
+            Main Footer Content
+          </div>
         </div>
         {/* Published by Purewater section */}
         <div className="bg-gray-300">
