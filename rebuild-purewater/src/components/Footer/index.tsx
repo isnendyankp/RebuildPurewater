101225bf4d214447a30378b93c314a30580c08db
isnendyankp <isnendyankp@gmail.com>
2024-06-11 05:17:07 +0700

    add path for link linkedin @footer

--- rebuild-purewater/src/components/Footer/index.tsx
+++ rebuild-purewater/src/components/Footer/index.tsx
@@ -89,7 +89,10 @@ const Footer = () => {
                     className="w-5 h-5"
                     viewBox="0 0 24 24"
                   >
-                    
+                    <path
+                      stroke="none"
+                      d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
+                    ></path>
                   </svg>
                 </Link>
               </span>
