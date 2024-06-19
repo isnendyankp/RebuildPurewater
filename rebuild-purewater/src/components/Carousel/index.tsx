f82f27589cf156b3fdf837d3419b362749944a8c
isnendyankp <isnendyankp@gmail.com>
2024-06-19 07:56:59 +0700

    Add data static for  outer wrapper carousel

--- rebuild-purewater/src/components/Carousel/index.tsx
+++ rebuild-purewater/src/components/Carousel/index.tsx
@@ -3,7 +3,11 @@ import React from 'react'
 const Carousel = () => {
   return (
     // Outer wrapper for carousel
-    <div id="indicators-carousel"  className="relative w-full">
+    <div
+      id="indicators-carousel"
+      className="relative w-full"
+      data-carousel="static"
+    >
       index
     </div>
   );
