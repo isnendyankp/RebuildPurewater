3ba0b98bd0aca23b402c2c0241080ea471613566
isnendyankp <isnendyankp@gmail.com>
2024-06-20 05:18:32 +0700

    Add Carousel 1

--- rebuild-purewater/src/components/Carousel/index.tsx
+++ rebuild-purewater/src/components/Carousel/index.tsx
@@ -10,7 +10,14 @@ const Carousel = () => {
     >
       {/* Carousel inner list */}
       <div className="relative h-screen overflow-hidden rounded-lg">
-
+        {/* Carousel 1 */}
+        <div className="carousel-item">
+          <img
+            src="https://cdn.tuk.dev/assets/templates/prodify/carousel-1.png"
+            alt="carousel-1"
+            className="object-cover w-full h-full"
+          />
+          </div>
       </div>
     </div>
   );
