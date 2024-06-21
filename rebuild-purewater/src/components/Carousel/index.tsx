61f98dc5c078eae5c194195e0851a3a0520734a3
isnendyankp <isnendyankp@gmail.com>
2024-06-22 06:13:59 +0700

    Add active data carousel item for div carousel 1

--- rebuild-purewater/src/components/Carousel/index.tsx
+++ rebuild-purewater/src/components/Carousel/index.tsx
@@ -12,7 +12,7 @@ const Carousel = () => {
       {/* Carousel inner list */}
       <div className="relative h-screen overflow-hidden rounded-lg">
         {/* Carousel 1 */}
-        <div className="duration-700 ease-in-out">
+        <div className="duration-700 ease-in-out" data-carousel-item="active">
           <Image
             className="absolute block object-cover w-full h-screen -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
             src={`/assets/carousel1.jpg`}
