1463edea1463443f176e54ca26a4bc6fb2a5b7cf
isnendyankp <isnendyankp@gmail.com>
2024-07-05 22:17:00 +0700

    add path for previous slider control

--- rebuild-purewater/src/components/Carousel/index.tsx
+++ rebuild-purewater/src/components/Carousel/index.tsx
@@ -121,7 +121,13 @@ const Carousel = () => {
             fill="none"
             viewBox="0 0 6 10"
           >
-           
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M5 1 1 5l4 4"
+            />
           </svg>
         </span>
       </button>
