7ac4745578a07d3bfff6599d3254c2e14afc9804
isnendyankp <isnendyankp@gmail.com>
2024-07-09 20:02:11 +0700

    refactor: Improve Carousel component with path next and previous slider controls

--- rebuild-purewater/src/components/Carousel/index.tsx
+++ rebuild-purewater/src/components/Carousel/index.tsx
@@ -147,7 +147,13 @@ const Carousel = () => {
             fill="none"
             viewBox="0 0 6 10"
           >
-            
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="m1 9 4-4-4-4"
+            />
           </svg>
         </span>
       </button>
