9c606c69b33533ead188f31a440816e6278ebe11
isnendyankp <isnendyankp@gmail.com>
2024-07-17 12:58:46 +0700

    feat: Add AboutUs component to home page

--- rebuild-purewater/src/pages/index.tsx
+++ rebuild-purewater/src/pages/index.tsx
@@ -1,12 +1,13 @@
 
 import Layout from '@/layout';
 import Carousel from '@/components/Carousel';
+import AboutUs from '@/components/AboutUs';
 
 export default function Home() {
   return (
     <Layout>
       <Carousel />
-      
+      <AboutUs />
     </Layout>
   );
 }
