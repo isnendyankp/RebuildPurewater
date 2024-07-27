819b0cf6796028e6e1fbfe2910e30dcd5ebafef7
isnendyankp <isnendyankp@gmail.com>
2024-07-28 06:22:01 +0700

    feat: Add WhyUs component to home page

--- rebuild-purewater/src/pages/index.tsx
+++ rebuild-purewater/src/pages/index.tsx
@@ -2,12 +2,14 @@
 import Layout from '@/layout';
 import Carousel from '@/components/Carousel';
 import AboutUs from '@/components/AboutUs';
+import WhyUs from '@/components/WhyUs';
 
 export default function Home() {
   return (
     <Layout>
       <Carousel />
       <AboutUs />
+      <WhyUs />
     </Layout>
   );
 }
