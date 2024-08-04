b9cd20e564fbf7f16977fad7766e81c3947ff362
isnendyankp <isnendyankp@gmail.com>
2024-08-04 08:26:52 +0700

    refactor: Update Login component with Formik integration and validation schema

--- rebuild-purewater/src/components/Login/index.tsx
+++ rebuild-purewater/src/components/Login/index.tsx
@@ -2,37 +2,38 @@ import React from 'react'
 import { useFormik } from 'formik'
 import * as yup from 'yup'
 
-const Login = () => {
-    // interface FormProps
-    interface FormProps {
-        email: string;
-        password: string;
-    }
+const Login = ({
+  onLoginSuccess,
+}: {
+  onLoginSuccess: (token: string) => void;
+}) => {
+  // interface FormProps
+  interface FormProps {
+    email: string;
+    password: string;
+  }
 
-    // useFormik hook
-    const formMik = useFormik<FormProps>({
-      initialValues: {
-        email: '',
-        password: '',
-      },
+  // useFormik hook
+  const formMik = useFormik<FormProps>({
+    initialValues: {
+      email: '',
+      password: '',
+    },
 
-      // validation for username, email & password
-      validationSchema: yup.object({
-        // name: yup.string().required('name tidak boleh kosong'),
-        email: yup
-          .string()
-          .email('Email tidak valid')
-          .required('Email tidak boleh kosong'),
-        password: yup
-          .string()
-          .min(8, 'Password minimal 8 karakter')
-          .required('Password tidak boleh kosong'),
-      }),
-
-    });
-  return (
-    <div>Login</div>
-  )
-}
+    // validation for username, email & password
+    validationSchema: yup.object({
+      // name: yup.string().required('name tidak boleh kosong'),
+      email: yup
+        .string()
+        .email('Email tidak valid')
+        .required('Email tidak boleh kosong'),
+      password: yup
+        .string()
+        .min(8, 'Password minimal 8 karakter')
+        .required('Password tidak boleh kosong'),
+    }),
+  });
+  return <div>Login</div>;
+};
 
 export default Login
