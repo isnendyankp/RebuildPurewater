7cc1b55c8b5f133fd63242ca73fd19ec7abcddd7
isnendyankp <isnendyankp@gmail.com>
2024-08-04 08:27:50 +0700

    refactor: Update Login component with Formik integration and validation schema

--- rebuild-purewater/src/components/Login/index.tsx
+++ rebuild-purewater/src/components/Login/index.tsx
@@ -7,7 +7,7 @@ const Login = ({
 }: {
   onLoginSuccess: (token: string) => void;
 }) => {
-  // interface FormProps
+  // interface for form props
   interface FormProps {
     email: string;
     password: string;
@@ -32,6 +32,38 @@ const Login = ({
         .min(8, 'Password minimal 8 karakter')
         .required('Password tidak boleh kosong'),
     }),
+
+    // onSubmit function for login form
+    onSubmit: async (values) => {
+      try {
+        const response = await fetch(
+          'https://revou-final-project-be-group-d-production.up.railway.app/login',
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(values),
+          }
+        );
+        // if response is ok, you can use it here
+        const data = await response.json();
+        // set token to local storage
+        localStorage.setItem('token', data.access_token);
+        // get token from local storage
+        const token = data.access_token;
+        // call onLoginSuccess function
+        onLoginSuccess(token);
+        // navigate to home page
+        // router.push('/');
+      } catch (error) {
+        // catch any network error or any error thrown in the fetch call
+        console.log(
+          'There has been a problem with your fetch operation: ' +
+            (error as Error).message
+        );
+      }
+    },
   });
   return <div>Login</div>;
 };
